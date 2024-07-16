b48bc2932bac6eeb218295de59acbaf0a09a632f
Sahana <160501512+sahanasani33@users.noreply.github.com>
2024-07-16 23:37:27 +0530

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -1,14 +1,13 @@
 provider "aws"{
 region="ap-south-1"
 }
-
-
-resource "aws_instance" "AWSEC2Instance"{
-ami = "ami-0cc9838aa7ab1dce7"
+ 
+resource "aws_instance" "AWSEC2"{
+ami = "ami-0ec0e125bb6c6e8ec"
 instance_type = "t2.micro"
-key_name = "devopsmssmay"
-security_groups = ["launch-wizard-2"]
-tags={
+key_name = "Amazonserver"
+security_group = ["launch-wizard-2"]
+tags = {
 Name = "terraformserver"
 }
 }
